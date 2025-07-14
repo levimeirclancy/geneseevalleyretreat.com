cf12c624e1de2f8966bb51d164c53e303b30a643
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-07-13 19:57:15 -0700

    Create index_home.php

---
+++ index_home.php
@@ -0,0 +1,30 @@
+<?
+
+if ($index_temp !== "valid"): exit; endif;
+
+$html_temp = null;
+
+	// 404 design
+
+$html_temp .= html_begin("Ziolicious", [ "style_hashtags", "style_global", "style_transitions" ]);
+
+$html_temp .= '<div class="body-tagline">Stories in <span class="body-tagline-primary-color">color</span>. Stories in <span class="body-tagline-secondary-color">context</span>.</div>';
+
+$html_temp .= '<div class="body-tagline-description">Immersive micro-stories showing whole, unapologetic Jewish life. All content is free for editorial use by Zionist creators. Still confused? <a href="/about/"><span class="link-animation">Learn more</span></a></div>';
+
+$html_temp .= '<main>';
+
+$html_temp .= '<div class="body-sections">';
+
+
+$html_temp .= 'dfgdsfgdsfg';
+
+$html_temp .= '</div>';
+
+$html_temp .= '</main>';
+
+$html_temp .= html_end();
+
+echo $html_temp;
+
+?>
