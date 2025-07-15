7b97b02a80e8dc809b6e6e7603d7b88a1e398c60
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-07-14 18:54:39 -0700

    Update index_home.php

--- index_home.php
+++ index_home.php
@@ -11,13 +11,13 @@ $html_temp .= html_begin("Genesee Valley Retreat", [ "style_global", "style_tran
 $html_temp .= '<div class="body-tagline">A <span class="body-tagline-colorized">return</span> to what <span class="body-tagline-colorized">matters</span>.</div>';
 
 $text_temp = 'In the heart of the Lost Sierra, Genesee Valley Retreat is more than a destination,';
-$html_temp .= '<div class="body-tagline-description-part-one">'.$text_temp.'</div>';
+$html_temp .= '<div class="body-tagline body-tagline-one">'.$text_temp.'</div>';
 
 $text_temp .= 'we are a place to disconnect from the noise and reconnect with the land, with others, and with yourself.';
-$html_temp .= '<div class="body-tagline-description-part-two">'.$text_temp.'</div>';
+$html_temp .= '<div class="body-tagline body-tagline-two">'.$text_temp.'</div>';
 
 $text_temp .= 'Here, amidst a healing landscape and a thriving four-acre food forest, we invite you to find your natural rhythm.';
-$html_temp .= '<div class="body-tagline-description-part-three">'.$text_temp.'</div>';
+$html_temp .= '<div class="body-tagline body-tagline-three">'.$text_temp.'</div>';
 
 $html_temp .= '<main>';
 
